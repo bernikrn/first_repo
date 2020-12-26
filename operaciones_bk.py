04d518b8219090b62ab277946bf527b1efcc7cad
bernikrn <bernardokoron@gmail.com>
2020-12-26 20:27:10 -0300

    operaciones agregado

---
+++ operaciones_bk.py
@@ -0,0 +1,9 @@
+#ingesos
+input_1 = int(input("ingrese un numero: "))
+input_2 = int(input("ingrese otro numero: "))
+
+#resulados
+print(input_1, " + ", input_2, " =", input_1+input_2)
+print(input_1, " - ", input_2, " =", input_1-input_2)
+print(input_1, " / ", input_2, " =", input_1/input_2)
+print(input_1, " * ", input_2, " =", input_1*input_2)
